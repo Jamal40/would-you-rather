0fb2b6ba55ada52b1bd945ebf328f476884f85da
Jamal40 <71548798+Jamal40@users.noreply.github.com>
2021-08-20 05:10:35 +0200

    Adding the leaderboard component

--- src/Components/Leaderboards/leaderboards.jsx
+++ src/Components/Leaderboards/leaderboards.jsx
@@ -2,12 +2,45 @@ import React, { Component } from "react";
 import UserCard from "../UserCard/user-card";
 import "./leaderboards.css";
 import "semantic-ui-css/semantic.min.css";
+import { getUsers } from "../../assets/api";
 
 export default class Leaderboards extends Component {
+  state = { topUsers: [] };
+  componentDidMount() {
+    getUsers().then((users) => {
+      const allUsers = [];
+      let i = 1;
+      for (let userId in users) {
+        allUsers.push(users[userId]);
+        i++;
+      }
+      allUsers.sort((f, s) => {
+        return (
+          Object.keys(s.answers).length +
+          s.questions.length -
+          (Object.keys(f.answers).length + f.questions.length)
+        );
+      });
+
+      this.setState({
+        topUsers: allUsers.slice(0, 3),
+      });
+    });
+  }
+
   render() {
     return (
       <div>
-        <UserCard />
+        {this.state.topUsers.map((user, index) => (
+          <UserCard
+            key={user.id}
+            rank={index + 1}
+            avatar={user.avatarURL}
+            userName={user.name}
+            answersCount={Object.keys(user.answers).length}
+            createdQuestionsCount={user.questions.length}
+          />
+        ))}
       </div>
     );
   }
--- src/Components/UserCard/user-card.jsx
+++ src/Components/UserCard/user-card.jsx
@@ -7,26 +7,25 @@ export default class UserCard extends Component {
   render() {
     return (
       <div className="user-card-container">
+        <p className="user-card-rank">{this.props.rank}</p>
         <div className="user-card-avatar">
-          <img
-            className="user-card-img"
-            src="https://picsum.photos/100"
-            alt=""
-          />
+          <img className={"user-card-img"} src={this.props.avatar} alt="" />
         </div>
         <div className="user-card-stats">
-          <p>Ahmed Ali</p>
+          <p>{this.props.userName}</p>
           <div className="user-card-stats-item">
             <h4>Answered Questions:</h4>
-            <h4>10</h4>
+            <h4>{this.props.answersCount}</h4>
           </div>
           <div className="user-card-stats-item">
             <h4>Created Questions:</h4>
-            <h4>10</h4>
+            <h4>{this.props.createdQuestionsCount}</h4>
           </div>
           <div className="user-card-stats-item user-card-score">
             <h4>Total Score:</h4>
-            <h4>10</h4>
+            <h4>
+              {this.props.answersCount + this.props.createdQuestionsCount}
+            </h4>
           </div>
         </div>
       </div>
