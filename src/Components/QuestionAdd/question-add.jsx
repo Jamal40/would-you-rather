7b118a07a8445c36d6cf81fcb41e41bee081d500
Jamal40 <71548798+Jamal40@users.noreply.github.com>
2021-08-20 02:34:11 +0200

    Finishing Adding Question component

--- src/Components/QuestionAdd/question-add.jsx
+++ src/Components/QuestionAdd/question-add.jsx
@@ -2,22 +2,68 @@ import React, { Component } from "react";
 import { Button, Checkbox, Form } from "semantic-ui-react";
 import "./question-add.css";
 import "semantic-ui-css/semantic.min.css";
+import { saveQuestion } from "../../assets/api";
+//Redux Imports
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
 
-export default class QuestionAdd extends Component {
+class QuestionAdd extends Component {
+  state = { firstOption: "", secondOption: "" };
+
+  addQuestion = () => {
+    const newQuestion = {
+      optionOneText: this.state.firstOption,
+      optionTwoText: this.state.secondOption,
+      author: this.props.currentUser.id,
+    };
+    saveQuestion(newQuestion).then(() => {
+      const inputs = document.querySelectorAll(".question-add-container input");
+      for (let i = 0; i < inputs.length; i++) {
+        inputs[i].value = "";
+      }
+      this.props.history.push("/");
+    });
+  };
+
+  firstOptionChanged = (e) => {
+    this.setState({
+      firstOption: e.target.value,
+    });
+  };
+  secondOptionChanged = (e) => {
+    this.setState({
+      secondOption: e.target.value,
+    });
+  };
+
   render() {
     return (
       <Form className="question-add-container">
         <h2>Would you rather...?</h2>
         <Form.Field>
           <label>First Option</label>
-          <input placeholder="First Option" />
+          <input
+            onChange={this.firstOptionChanged}
+            placeholder="First Option"
+          />
         </Form.Field>
         <Form.Field>
           <label>Second Option</label>
-          <input placeholder="Second Option" />
+          <input
+            onChange={this.secondOptionChanged}
+            placeholder="Second Option"
+          />
         </Form.Field>
-        <Button color="blue">Blue</Button>
+        <Button onClick={this.addQuestion} color="blue">
+          Blue
+        </Button>
       </Form>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return { currentUser: state.userReducer };
+};
+
+export default withRouter(connect(mapStateToProps)(QuestionAdd));
