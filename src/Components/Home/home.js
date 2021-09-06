c5ae069cef5ac7a2bd521cb219f6eb9bb6d27300
Jamal40 <71548798+Jamal40@users.noreply.github.com>
2021-09-06 15:46:12 +0200

    Updating UI

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -54,7 +54,7 @@ class Home extends Component {
     this.updateFromReduxState();
     return this.props.allQuestions ? (
       <div className="home-container">
-        <Menu pointing>
+        <Menu className="home-menu" pointing>
           <Menu.Item
             className="home-item"
             name="answered"
@@ -68,7 +68,7 @@ class Home extends Component {
             onClick={this.handleItemClick}
           />
         </Menu>
-        <Segment>
+        <Segment className="home-segment">
           {activeItem === "unanswered" ? (
             <div>
               <Card.Group className="home-questions-container">
